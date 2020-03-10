dbd0e4699fa0299559e3693f17b3194fd08fe66d
nhihieu <nhihieu0305@gmail.com>
2020-03-11 00:15:21 +0700

    source code

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from .model import *
+
+if __name__ == "__main__":
+    # do something
+    pass
---
+++ src/model.py
@@ -0,0 +1,9 @@
+# Create model for train and test
+
+
+class Train:
+    pass
+
+
+class Test:
+    pass
