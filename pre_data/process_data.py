fd98fab5091923551d7c86415f756eb852322864
Trần Phương Nam <tnam54478@gmail.com>
2020-03-14 03:02:25 +0700

    update readme

--- pre_data/process_data.py
+++ pre_data/process_data.py
@@ -1,1 +1,18 @@
-# handle data
+import numpy as np
+import librosa
+from   src.config import *
+
+def wav_to_mel_spectrogram(wav):
+    pass
+
+
+def trim_silences(wav):
+    pass
+
+
+def normalize_volume(wav):
+    pass
+
+
+def main_process(path_wav):
+    pass
